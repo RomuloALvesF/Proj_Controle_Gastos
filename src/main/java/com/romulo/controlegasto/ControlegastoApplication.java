fb3d38a2793e5b59d0fb180f9f6de2d2eb4f1274
RomuloALvesF <romuloalves121@gmail.com>
2024-11-14 10:20:08 -0300

    Criação do projeto

---
+++ src/main/java/com/romulo/controlegasto/ControlegastoApplication.java
@@ -0,0 +1,13 @@
+package com.romulo.controlegasto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlegastoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlegastoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romulo/controlegasto/ControlegastoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romulo.controlegasto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlegastoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
